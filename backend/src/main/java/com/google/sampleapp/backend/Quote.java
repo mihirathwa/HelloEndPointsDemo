20731d624fb2edc3bde431ad1e56394f430850d9
Mihir Rathwa <mihir.rathwa@gmail.com>
2017-05-01 16:51:47 -0700

    init

---
+++ backend/src/main/java/com/google/sampleapp/backend/Quote.java
@@ -0,0 +1,35 @@
+package com.google.sampleapp.backend;
+
+/**
+ * Created by Mihir on 04/30/2017.
+ */
+
+public class Quote {
+    int id;
+    String who;
+    String what;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWho() {
+        return who;
+    }
+
+    public void setWho(String who) {
+        this.who = who;
+    }
+
+    public String getWhat() {
+        return what;
+    }
+
+    public void setWhat(String what) {
+        this.what = what;
+    }
+}
